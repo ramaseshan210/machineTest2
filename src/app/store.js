78686cbf3d85e96d66efcc8784b2175dc3668517
ramaseshan210 <ramannp210@gmail.com>
2025-10-25 10:27:59 +0530

    error-fixed_store.js_updated- login succesfull

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 // src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
-// import formReducer from '../features/form/formSlice';
+import formReducer from '../features/form/formSlice';
 
 
 export const store = configureStore({
   reducer: {
- 
+    form: formReducer
   }
 });
